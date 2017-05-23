acbd52bc1a8f3d318b1c945eb9864286ff580864
Marian Dziubiak <marian.dziubiak@gmail.com>
2017-05-23 06:00:32 +0200

    Add superuser and models to admin

--- wyboryApp/admin.py
+++ wyboryApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+myModels = [Kandydat, Okreg, Obwod, Gmina, Wynik]  # iterable list
+
+admin.site.register(myModels)
